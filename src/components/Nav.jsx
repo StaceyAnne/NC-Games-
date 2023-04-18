0e2936779d0841305adfe745447e723d23f763e7
staceyanne <staceywhite10@gmail.com>
2023-04-18 11:47:23 +0100

    added view all reviews link to homepage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,14 @@
-
+import { Link } from "react-router-dom"
 import { useState } from "react";
 
 const Nav = () => {
  const [category, setCategory] = useState("")
+
+ return (
+   <Link to="/reviews">
+    <p>View all reviews</p>
+  </Link>
+ )
 }
 
 export default Nav; 
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,42 +1,38 @@
 import { useState, useEffect } from "react";
-import { getReviews } from '../api';
-import ReviewCard from '../components/ReviewCard'
+import { getReviews } from "../api";
+import ReviewCard from "../components/ReviewCard";
 
 const Reviews = ({ category }) => {
-    const [reviews, setReviews] = useState([])
-    const [loading, setLoading] = useState(true)
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
+    getReviews(category).then(({ reviews }) => {
+      setReviews(reviews);
+      setLoading(false);
+    });
+  }, [category]);
 
-        getReviews(category).then(({ reviews }) => {
+  if (loading) {
+    return <p>Loading....</p>;
+  }
 
-            setReviews(reviews)
-            setLoading(false)
+  return (
+    <section>
+      <button className="sortCategory">Sort by cateogory</button>
+      <button className="sort">Sort by</button>
+      <p>Showing: all reviews</p>
+      <ul className="reviewList">
+        {reviews.map((review) => {
+          return (
+            <li key={review.review_id}>
+              <ReviewCard review={review} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
 
-
-        })
-    }, [category])
-
-    if (loading) {
-        return <p>Loading....</p>
-    }
-
-
-
-    return (
-        <section>
-            <button className="sortCategory">Sort by cateogory</button>
-            <button className='sort'>Sort by</button>
-            <p>Showing: all reviews</p>
-            <ul className="reviewList">
-                {reviews.map((review) => {
-                    return <li key={review.review_id}><ReviewCard review={review} /></li>
-                })}
-            </ul>
-         
-        </section>
-    )
-}
-
-
-export default Reviews; 
+export default Reviews;
