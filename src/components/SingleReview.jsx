e0f30b1716abde46ce4300d3a1c2ebd417819404
staceyanne <staceywhite10@gmail.com>
2023-04-18 17:30:13 +0100

    added single review functionality

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,39 +1,62 @@
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { getReviewById } from '../api';
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getReviewById } from "../api";
+import { Link } from "react-router-dom";
 
 const SingleReview = () => {
-    const [singleReview, setSingleReview] = useState("")
-    const { review_id } = useParams();
+  const [singleReview, setSingleReview] = useState("");
+  const { review_id } = useParams();
+  const [loading, setLoading] = useState(true);
 
-   useEffect(() => {
+  useEffect(() => {
     getReviewById(review_id).then((response) => {
-        setSingleReview(response)
-})
-   })
-   console.log(singleReview)
-    return (
-        <section className="singleReview">
-    <h3>{singleReview.title} by {singleReview.owner}</h3>
-    <img src={singleReview.review_img_url} alt={singleReview.title}></img>
-    <p>{singleReview.review_body}</p>
-    <p>Votes: {singleReview.votes}</p>
-    <form>
-        <label htmlFor="vote">Add vote</label>
-        <input type='number' name="vote"></input>
-        <button>Submit vote</button>
-    </form>
-    <p>{singleReview.comment_count} comments</p>
-    <form>
-        <label htmlFor="comment">Add a comment: </label>
-        <input type="text" name="comment"></input>
-        <button>Submit comment</button>
-    </form>
-    <button>Previous review</button>
-    <button>Next review</button>
-    </section>
-    )
-}
+      setSingleReview(response);
+      setLoading(false);
+    });
+  });
 
-export default SingleReview; 
+  if (loading) return <p>Loading...</p>;
+
+  const date = new Date(singleReview.created_at);
+  const newDate = date.toString().split("GMT");
+  const formattedDate = newDate[0];
+
+  return (
+    <div className="singleItem">
+      <section className="singleReview">
+        <h2>{singleReview.title}</h2>
+        <div className="singleReviewBody">
+          <img src={singleReview.review_img_url} alt={singleReview.title}></img>
+          <p className="reviewContent">{singleReview.review_body}</p>
+        </div>
+        <p className="author">
+          Written by: {<Link to="/users">{singleReview.owner}</Link>} on{" "}
+          {formattedDate}
+        </p>
+        <div className="reviewInfo">
+          <div className="voteContainer">
+            <p>Votes: {singleReview.votes}</p>
+            <form>
+              <label htmlFor="vote">Add vote</label>
+              <input type="number" name="vote"></input>
+              <button>Submit vote</button>
+            </form>
+          </div>
+          <div className="commentDiv">
+            <p>{singleReview.comment_count} comments</p>
+            <form>
+              <label htmlFor="comment">Add a comment: </label>
+              <input type="text" name="comment"></input>
+              <button>Submit comment</button>
+            </form>
+          </div>
+        </div>
+        <Link to="/">
+          <button className="backReviews">Back to reviews</button>
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default SingleReview;
