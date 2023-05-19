63253adf84871064f2c589c6cb754889e47f8f2c
staceyanne <staceywhite10@gmail.com>
2023-05-19 21:21:56 +0100

    added responsivenes to landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,53 +2,54 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getCategories, getReviews } from "../api";
 import ReviewCard from "./ReviewCard";
-import  image  from './category.png'
+import image from "./category.png";
 
 const Nav = () => {
   const [categories, setCategories] = useState([]);
-  const [reviews, setReviews] = useState([]); 
-  const [loading, setLoading] = useState(true)
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-
-  // need to regactor into promise all 
+  // need to refactor into promise all
   useEffect(() => {
     getCategories().then((categories) => {
       setCategories(categories);
     });
 
     getReviews().then(({ reviews }) => {
-    
       const topReviews = reviews.filter((review, index) => {
         return index <= 5;
       });
       setReviews(topReviews);
-      
-  });
+    });
 
-  setLoading(false)
+    setLoading(false);
+  }, []);
 
-}, [])
-
-if (loading) return <p>Loading....</p>
+  if (loading) return <p>Loading....</p>;
 
   return (
     <div className="landing">
-      <div> 
+      <div>
         <ul className="sideBar">
           {categories.map((category, index) => {
             return (
               <Link to="/reviews">
-                <li key={index}><div className="categoryItem"><img src={image}></img>{category.slug}</div></li>
+                <li key={index}>
+                  <div className="categoryItem">
+                    <img src={image} className="catImage"></img>
+                    {category.slug}
+                  </div>
+                </li>
               </Link>
             );
           })}
         </ul>
       </div>
       <div className="landingContent">
-          <Link to="/reviews">
-        <button className="viewReviewsButton">View all reviews</button>
-     </Link>
-          <h3>Most recent reviews</h3>
+        <Link to="/reviews">
+          <button className="viewReviewsButton">View all reviews</button>
+        </Link>
+        <h3>Most recent reviews</h3>
         <ul className="landingContentList">
           {reviews.map((review) => {
             return (
